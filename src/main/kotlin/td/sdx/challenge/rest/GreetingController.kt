c93f37cd162b6ca9478eeeb476158e74b0e30ff2
Leandro Zago <leandrozago.dev@gmail.com>
2021-11-08 14:08:07 -0200

    Add a greeting controller that returns a hello message

---
+++ src/main/kotlin/td/sdx/challenge/rest/GreetingController.kt
@@ -0,0 +1,16 @@
+package td.sdx.challenge.rest
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class GreetingController {
+
+    @GetMapping("/{name}")
+    fun sayHelloTo(@PathVariable name : String) : String{
+        return "Hello $name"
+    }
+}
