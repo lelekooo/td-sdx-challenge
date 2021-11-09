6a3efe8e714075de1972fad865b5c329e58c568c
Leandro Zago <leandrozago.dev@gmail.com>
2021-11-09 06:42:37 -0200

    Fix PR suggestion

--- src/main/kotlin/td/sdx/challenge/rest/GreetingController.kt
+++ src/main/kotlin/td/sdx/challenge/rest/GreetingController.kt
@@ -1,16 +1,15 @@
 package td.sdx.challenge.rest
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/greeting")
 class GreetingController {
 
-    @GetMapping("/{name}")
-    fun sayHelloTo(@PathVariable name : String) : String{
-        return "Hello $name"
-    }
+    @GetMapping
+    fun sayHelloTo(@RequestParam name : String) = "Hello $name"
+
 }
